4ae4bb0fb43a96fd36541926c49ba9fe0ae1b0ca
Liam Forde <liam.forde@hotmail.co.uk>
2017-03-21 22:10:13 +0000

    setting up D3DManager

--- ClusteredRendering/inc/Direct3DManager.h
+++ ClusteredRendering/inc/Direct3DManager.h
@@ -7,6 +7,32 @@ public:
 	Direct3DManager(const Direct3DManager&);
 	~Direct3DManager();
 
-	bool Initialize();
+	bool Initialize(LONG,LONG, bool, HWND, bool, float, float);
 	void Unitialize();
+
+	void Clear(const FLOAT[4], FLOAT, UINT8);
+	void SetRasterizerStage();
+	void Present(bool);
+
+	DXGI_RATIONAL QueryRefreshRate(UINT,UINT, BOOL);
+
+	ID3D11Device* GetDevice();
+	ID3D11DeviceContext* GetDeviceContext();
+
+	void GetProjectionMatrix(XMMATRIX&);
+	void GetWorldMatrix(XMMATRIX&);
+	void GetOrthoMatrix(XMMATRIX&);
+
+private:
+	bool m_isVSyncEnabled;
+	IDXGISwapChain* m_swapChain;
+	ID3D11Device* m_device;
+	ID3D11DeviceContext* m_deviceContext;
+	ID3D11RenderTargetView* m_renderTargetView;
+	ID3D11DepthStencilView* m_depthStencilView;
+	ID3D11Texture2D* m_depthStencilBuffer;
+	ID3D11DepthStencilState* m_depthStencilState;
+	ID3D11RasterizerState* m_rasterizerState;
+	D3D11_VIEWPORT m_viewport;
+
 };
--- ClusteredRendering/src/Direct3DManager.cpp
+++ ClusteredRendering/src/Direct3DManager.cpp
@@ -3,6 +3,16 @@
 
 Direct3DManager::Direct3DManager()
 {
+	m_swapChain = nullptr;
+	m_device = nullptr;
+	m_deviceContext = nullptr;
+	m_renderTargetView = nullptr;
+	m_depthStencilView = nullptr;
+	m_depthStencilBuffer = nullptr;
+	m_depthStencilState = nullptr;
+	m_rasterizerState = nullptr;
+	m_viewport = { 0 };
+
 }
 
 Direct3DManager::Direct3DManager(const Direct3DManager&)
@@ -13,11 +23,302 @@ Direct3DManager::~Direct3DManager()
 {
 }
 
-bool Direct3DManager::Initialize()
+bool Direct3DManager::Initialize(LONG windowWidth, LONG windowHeight, bool vsync, HWND windowHandle, 
+	bool fullscreen, float screenDepth, float screenNear)
 {
-	return false;
+	// A window handle must have been created already.
+	assert(windowHandle != 0);
+
+	RECT clientRect;
+	GetClientRect(windowHandle, &clientRect);
+
+	// Compute the exact client dimensions. This will be used
+	// to initialize the render targets for our swap chain.
+	unsigned int clientWidth = clientRect.right - clientRect.left;
+	unsigned int clientHeight = clientRect.bottom - clientRect.top;
+
+	DXGI_SWAP_CHAIN_DESC swapChainDesc;
+	ZeroMemory(&swapChainDesc, sizeof(DXGI_SWAP_CHAIN_DESC));
+
+	swapChainDesc.BufferCount = 1;
+	swapChainDesc.BufferDesc.Width = clientWidth;
+	swapChainDesc.BufferDesc.Height = clientHeight;
+	swapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	swapChainDesc.BufferDesc.RefreshRate = QueryRefreshRate(clientWidth, clientHeight, vsync);
+	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	swapChainDesc.OutputWindow = windowHandle;
+	swapChainDesc.SampleDesc.Count = 1;
+	swapChainDesc.SampleDesc.Quality = 0;
+	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+	swapChainDesc.Windowed = TRUE;
+
+	UINT createDeviceFlags = 0;
+#if _DEBUG
+	createDeviceFlags = D3D11_CREATE_DEVICE_DEBUG;
+#endif
+
+	// These are the feature levels that we will accept.
+	D3D_FEATURE_LEVEL featureLevels[] =
+	{
+		D3D_FEATURE_LEVEL_11_1,
+		D3D_FEATURE_LEVEL_11_0,
+		D3D_FEATURE_LEVEL_10_1,
+		D3D_FEATURE_LEVEL_10_0,
+		D3D_FEATURE_LEVEL_9_3,
+		D3D_FEATURE_LEVEL_9_2,
+		D3D_FEATURE_LEVEL_9_1
+	};
+
+	// This will be the feature level that 
+	// is used to create our device and swap chain.
+	D3D_FEATURE_LEVEL featureLevel;
+
+	HRESULT hr = D3D11CreateDeviceAndSwapChain(nullptr, D3D_DRIVER_TYPE_HARDWARE,
+		nullptr, createDeviceFlags, featureLevels, _countof(featureLevels),
+		D3D11_SDK_VERSION, &swapChainDesc, &m_swapChain, &m_device, &featureLevel,
+		&m_deviceContext);
+
+	if (hr == E_INVALIDARG)
+	{
+		hr = D3D11CreateDeviceAndSwapChain(nullptr, D3D_DRIVER_TYPE_HARDWARE,
+			nullptr, createDeviceFlags, &featureLevels[1], _countof(featureLevels) - 1,
+			D3D11_SDK_VERSION, &swapChainDesc, &m_swapChain, &m_device, &featureLevel,
+			&m_deviceContext);
+	}
+
+	if (FAILED(hr))
+	{
+		return -1;
+	}
+
+	// The Direct3D device and swap chain were successfully created.
+	// Now we need to initialize the buffers of the swap chain.
+	// Next initialize the back buffer of the swap chain and associate it to a 
+	// render target view.
+	ID3D11Texture2D* backBuffer;
+	hr = m_swapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&backBuffer);
+	if (FAILED(hr))
+	{
+		return -1;
+	}
+
+	hr = m_device->CreateRenderTargetView(backBuffer, nullptr, &m_renderTargetView);
+	if (FAILED(hr))
+	{
+		return -1;
+	}
+
+	SafeRelease(backBuffer);
+
+	// Create the depth buffer for use with the depth/stencil view.
+	D3D11_TEXTURE2D_DESC depthStencilBufferDesc;
+	ZeroMemory(&depthStencilBufferDesc, sizeof(D3D11_TEXTURE2D_DESC));
+
+	depthStencilBufferDesc.ArraySize = 1;
+	depthStencilBufferDesc.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+	depthStencilBufferDesc.CPUAccessFlags = 0; // No CPU access required.
+	depthStencilBufferDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	depthStencilBufferDesc.Width = clientWidth;
+	depthStencilBufferDesc.Height = clientHeight;
+	depthStencilBufferDesc.MipLevels = 1;
+	depthStencilBufferDesc.SampleDesc.Count = 1;
+	depthStencilBufferDesc.SampleDesc.Quality = 0;
+	depthStencilBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+
+	hr = m_device->CreateTexture2D(&depthStencilBufferDesc, nullptr, &m_depthStencilBuffer);
+	if (FAILED(hr))
+	{
+		return -1;
+	}
+
+	hr = m_device->CreateDepthStencilView(m_depthStencilBuffer, nullptr, &m_depthStencilView);
+	if (FAILED(hr))
+	{
+		return -1;
+	}
+
+	// Setup depth/stencil state.
+	D3D11_DEPTH_STENCIL_DESC depthStencilStateDesc;
+	ZeroMemory(&depthStencilStateDesc, sizeof(D3D11_DEPTH_STENCIL_DESC));
+
+	depthStencilStateDesc.DepthEnable = TRUE;
+	depthStencilStateDesc.DepthWriteMask = D3D11_DEPTH_WRITE_MASK_ALL;
+	depthStencilStateDesc.DepthFunc = D3D11_COMPARISON_LESS;
+	depthStencilStateDesc.StencilEnable = FALSE;
+
+	hr = m_device->CreateDepthStencilState(&depthStencilStateDesc, &m_depthStencilState);
+
+	// Setup rasterizer state.
+	D3D11_RASTERIZER_DESC rasterizerDesc;
+	ZeroMemory(&rasterizerDesc, sizeof(D3D11_RASTERIZER_DESC));
+
+	rasterizerDesc.AntialiasedLineEnable = FALSE;
+	rasterizerDesc.CullMode = D3D11_CULL_BACK;
+	rasterizerDesc.DepthBias = 0;
+	rasterizerDesc.DepthBiasClamp = 0.0f;
+	rasterizerDesc.DepthClipEnable = TRUE;
+	rasterizerDesc.FillMode = D3D11_FILL_SOLID;
+	rasterizerDesc.FrontCounterClockwise = FALSE;
+	rasterizerDesc.MultisampleEnable = FALSE;
+	rasterizerDesc.ScissorEnable = FALSE;
+	rasterizerDesc.SlopeScaledDepthBias = 0.0f;
+
+	// Create the rasterizer state object.
+	hr = m_device->CreateRasterizerState(&rasterizerDesc, &m_rasterizerState);
+	if (FAILED(hr))
+	{
+		return -1;
+	}
+
+	// Initialize the viewport to occupy the entire client area.
+	m_viewport.Width = static_cast<float>(clientWidth);
+	m_viewport.Height = static_cast<float>(clientHeight);
+	m_viewport.TopLeftX = 0.0f;
+	m_viewport.TopLeftY = 0.0f;
+	m_viewport.MinDepth = 0.0f;
+	m_viewport.MaxDepth = 1.0f;
+
+	return 0;
 }
 
 void Direct3DManager::Unitialize()
 {
 }
+
+
+void Direct3DManager::Clear(const FLOAT clearColor[4], FLOAT clearDepth, UINT8 clearStencil)
+{
+	m_deviceContext->ClearRenderTargetView(m_renderTargetView, clearColor);
+	m_deviceContext->ClearDepthStencilView(m_depthStencilView, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, clearDepth, clearStencil);
+}
+
+void Direct3DManager::SetRasterizerStage()
+{
+	m_deviceContext->RSSetState(m_rasterizerState);
+	m_deviceContext->RSSetViewports(1, &m_viewport);
+
+}
+
+void Direct3DManager::Present(bool vsync)
+{
+	if (vsync)
+	{
+		m_swapChain->Present(1, 0);
+	}
+	else
+	{
+		m_swapChain->Present(0, 0);
+	}
+}
+
+DXGI_RATIONAL Direct3DManager::QueryRefreshRate(UINT screenWidth, UINT screenHeight, BOOL vsync)
+{
+	DXGI_RATIONAL refreshRate = { 0,1 };
+	if (vsync)
+	{
+		IDXGIFactory* factory;
+		IDXGIAdapter* adapter;
+		IDXGIOutput* adapterOutput;
+		DXGI_MODE_DESC* displayModeList;
+
+		// Create a DirectX graphics interface factory
+		HRESULT hr = CreateDXGIFactory(__uuidof(IDXGIFactory), (void**)&factory);
+		if (FAILED(hr))
+		{
+			MessageBox(0,
+				TEXT("Could not create DXGIFactory instance"),
+				TEXT("Error"),
+				MB_OK);
+
+			throw new std::exception("Failedto create DXGIFactory");
+		}
+
+		hr = factory->EnumAdapters(0, &adapter);
+		if (FAILED(hr))
+		{
+			MessageBox(0,
+				TEXT("Failed to enumerate adapters."),
+				TEXT("Query Refresh Rate"),
+				MB_OK);
+
+			throw new std::exception("Failed to enumerate adapters.");
+		}
+
+		hr = adapter->EnumOutputs(0, &adapterOutput);
+		if (FAILED(hr))
+		{
+			MessageBox(0,
+				TEXT("Failed to enumerate adapter outputs"),
+				TEXT("Query Refresh Rate"),
+				MB_OK);
+
+			throw new std::exception("Failed to enumerate adapter outputs");
+		}
+
+		UINT numDisplayModes;
+		hr = adapterOutput->GetDisplayModeList(DXGI_FORMAT_B8G8R8A8_UNORM, DXGI_ENUM_MODES_INTERLACED, &numDisplayModes, nullptr);
+		if (FAILED(hr))
+		{
+			MessageBox(0,
+				TEXT("Failed to query display mode list"),
+				TEXT("Query Refresh Rate"),
+				MB_OK);
+
+			throw new std::exception("Failed to query display mode list");
+		}
+
+		displayModeList = new DXGI_MODE_DESC[numDisplayModes];
+		assert(displayModeList);
+
+		hr = adapterOutput->GetDisplayModeList(DXGI_FORMAT_B8G8R8A8_UNORM, DXGI_ENUM_MODES_INTERLACED, &numDisplayModes, displayModeList);
+		if (FAILED(hr))
+		{
+			MessageBox(0,
+				TEXT("Failed to query display mode list"),
+				TEXT("Query Refresh Rate"),
+				MB_OK);
+
+			throw new std::exception("Failed to query display mode list");
+		}
+
+		// Now store the refresh rate of the monitor that matches the width and height of the requested screen
+		for (UINT i = 0; i < numDisplayModes; i++)
+		{
+			if (displayModeList[i].Width == screenWidth && displayModeList[i].Height == screenHeight)
+			{
+				refreshRate = displayModeList[i].RefreshRate;
+			}
+		}
+
+		delete[] displayModeList;
+		SafeRelease(adapterOutput);
+		SafeRelease(adapter);
+		SafeRelease(factory);
+	}
+
+	return refreshRate;
+}
+
+ID3D11Device* Direct3DManager::GetDevice()
+{
+	return m_device;
+}
+
+ID3D11DeviceContext* Direct3DManager::GetDeviceContext()
+{
+	return  m_deviceContext;
+}
+
+void Direct3DManager::GetProjectionMatrix(XMMATRIX& projectionMatrix)
+{
+	projectionMatrix = m_projectionMatrix;
+}
+
+void Direct3DManager::GetWorldMatrix(XMMATRIX&)
+{
+}
+
+void Direct3DManager::GetOrthoMatrix(XMMATRIX&)
+{
+}
+
